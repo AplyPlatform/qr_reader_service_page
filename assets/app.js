7aab67f7a8285c8981703b210655dd87e216fd73
gunman_PC_HOME_DESKTOP <gunman97@gmail.com>
2023-11-02 13:43:55 +0900

    레이아웃 개선, referrer 가 있으면 history.back 하게끔 개선

--- assets/app.js
+++ assets/app.js
@@ -1,9 +1,28 @@
-window.addEventListener('DOMContentLoaded', function () {
+window.addEventListener('DOMContentLoaded', async function () {
     if (!('mediaDevices' in navigator &&
         'getUserMedia' in navigator.mediaDevices &&
         'Worker' in window)) {
-        alert('Sorry, your browser is not compatible with this app.');
+        alert('카메라를 지원하지 않는 환경입니다. 이전 화면으로 돌아갑니다.');
+        history.back();
         return;
+    }
+
+   
+    let ret = await getMedia();
+    if (ret == false) {
+        alert('카메라를 지원하지 않는 환경이거나 카메라 사용권한을 취소하셨습니다. 이전 화면으로 돌아갑니다.\n\n다시 사용하시려면 브라우저의 새탭에서 접속해주세요.');
+        history.back();
+        return;
+    }
+
+    $("#wait_text").hide();
+    $("#center_scan_text").show();
+
+    if (document.referrer != "") {
+        $("#back_button_area").show();
+        $("#back_button_area").click(function() {
+            history.back();
+        });
     }
 
     // html elements    
@@ -43,4 +62,29 @@ window.addEventListener('DOMContentLoaded', function () {
 function vibrate() {
     const canVibrate = window.navigator.vibrate;
     if (canVibrate) window.navigator.vibrate([10,10,5,5]);
+}
+
+
+async function getMedia() {
+    let stream = null;
+  
+    try {
+      stream = await navigator.mediaDevices.getUserMedia({
+        audio: false,
+        video: true,
+      });
+      /* use the stream */
+    } catch (err) {
+      /* handle the error */
+
+      return false;
+    }
+
+    stream.getTracks().forEach((track) => {
+        if (track.readyState == 'live' && track.kind === 'video') {
+            track.stop();
+        }
+    });
+
+    return true;
 }
