c411cd22d8bc53b32a513f74963ac74ce7586a02
gunman_PC_HOME_DESKTOP <gunman97@gmail.com>
2023-11-02 14:44:37 +0900

    카메라 권한 확인, 오류 대응 방법 개선 > 속도 개선

--- assets/app.js
+++ assets/app.js
@@ -1,20 +1,4 @@
 window.addEventListener('DOMContentLoaded', async function () {
-    if (!('mediaDevices' in navigator &&
-        'getUserMedia' in navigator.mediaDevices &&
-        'Worker' in window)) {
-        alert('카메라를 지원하지 않는 환경입니다. 이전 화면으로 돌아갑니다.');
-        history.back();
-        return;
-    }
-
-   
-    let ret = await getMedia();
-    if (ret == false) {
-        alert('카메라를 지원하지 않는 환경이거나 카메라 사용권한을 취소하셨습니다. 이전 화면으로 돌아갑니다.\n\n다시 사용하시려면 브라우저의 새탭에서 접속해주세요.');
-        history.back();
-        return;
-    }
-
     $("#wait_text").hide();
     $("#center_scan_text").show();
 
@@ -24,9 +8,8 @@ window.addEventListener('DOMContentLoaded', async function () {
             history.back();
         });
     }
-
-    // html elements    
-    const waitScreen = document.getElementById('wait-screen');            
+    
+    const waitScreen = document.getElementById('wait-screen');
     const qrArea = document.getElementById('qr-reader');
     const centerScanText = document.getElementById('center_scan_text');        
     
@@ -52,39 +35,25 @@ window.addEventListener('DOMContentLoaded', async function () {
             }
         }
     }
-
+    
     var html5QrCode = new Html5Qrcode("qr-reader");
+    if (html5QrCode == null) {
+        alert('카메라를 지원하지 않는 환경이거나 카메라 사용권한을 취소하셨습니다. 이전 화면으로 돌아갑니다.\n\n다시 사용하시려면 브라우저의 새탭에서 접속해주세요.');
+        history.back();
+        return;
+    }
     const config = { fps: 10, qrbox: { width: 200, height: 200 } };
 
-    html5QrCode.start({ facingMode: "environment" }, config, qrCodeSuccessCallback);
+    html5QrCode.start({ facingMode: "environment" }, config, qrCodeSuccessCallback)
+        .then((ignore) => {
+        })
+        .catch((err) => {
+            alert('카메라를 지원하지 않는 환경이거나 카메라 사용권한을 취소하셨습니다. 이전 화면으로 돌아갑니다.\n\n다시 사용하시려면 브라우저의 새탭에서 접속해주세요.');
+            history.back();
+        });    
 });
 
 function vibrate() {
     const canVibrate = window.navigator.vibrate;
     if (canVibrate) window.navigator.vibrate([10,10,5,5]);
-}
-
-
-async function getMedia() {
-    let stream = null;
-  
-    try {
-      stream = await navigator.mediaDevices.getUserMedia({
-        audio: false,
-        video: true,
-      });
-      /* use the stream */
-    } catch (err) {
-      /* handle the error */
-
-      return false;
-    }
-
-    stream.getTracks().forEach((track) => {
-        if (track.readyState == 'live' && track.kind === 'video') {
-            track.stop();
-        }
-    });
-
-    return true;
 }
