5cbc43a8d373c6338b8767e861795bdd98cc7f12
gunman_PC_HOME_DESKTOP <gunman97@gmail.com>
2023-11-03 21:04:37 +0900

    햅틱 추가, gaevet 추가

--- assets/app.js
+++ assets/app.js
@@ -5,6 +5,8 @@ window.addEventListener('DOMContentLoaded', async function () {
     if (document.referrer != "") {
         $("#back_button_area").show();
         $("#back_button_area").click(function() {
+            GA_EVENT("back_button", "click", "service");
+            vibrate();
             history.back();
         });
     }
@@ -22,6 +24,7 @@ window.addEventListener('DOMContentLoaded', async function () {
             centerScanText.style.display = "none";            
 
             try {
+                GA_EVENT("qr_read_" + decodedText, decodedText, "service");
                 setTimeout(() => {location.href = decodedText;}, 2000);
             }
             catch (e) {
@@ -38,7 +41,8 @@ window.addEventListener('DOMContentLoaded', async function () {
     
     var html5QrCode = new Html5Qrcode("qr-reader");
     if (html5QrCode == null) {
-        alert('카메라를 지원하지 않는 환경이거나 카메라 사용권한을 취소하셨습니다. 이전 화면으로 돌아갑니다.\n\n다시 사용하시려면 브라우저의 새 탭에서 접속해 주세요.');
+        GA_EVENT("html5QrCode_error_1", "service", "service");
+        alert('카메라를 지원하지 않는 환경이거나 카메라 사용권한을 취소하셨습니다. 이전 화면으로 돌아갑니다.\n\n다시 사용하시려면 브라우저의 새 탭에서 접속해 주세요.');        
         history.back();
         return;
     }
@@ -48,12 +52,24 @@ window.addEventListener('DOMContentLoaded', async function () {
         .then((ignore) => {
         })
         .catch((err) => {
-            alert('카메라를 지원하지 않는 환경이거나 카메라 사용권한을 취소하셨습니다. 이전 화면으로 돌아갑니다.\n\n다시 사용하시려면 브라우저의 새 탭에서 접속해 주세요.');
+            GA_EVENT("html5QrCode_error_2", "service", "service");
+            alert('카메라를 지원하지 않는 환경이거나 카메라 사용권한을 취소하셨습니다. 이전 화면으로 돌아갑니다.\n\n다시 사용하시려면 브라우저의 새 탭에서 접속해 주세요.');            
             history.back();
-        });    
+        });
 });
 
 function vibrate() {
     const canVibrate = window.navigator.vibrate;
     if (canVibrate) window.navigator.vibrate([10,10,5,5]);
+}
+
+function GA_EVENT(event_name, event_target_name, event_label) {
+    if (typeof gtag !== 'undefined') {
+      gtag(
+          'event', event_name, {
+            'event_category': event_target_name,
+            'event_label': event_label
+          }
+      );
+    }
 }
